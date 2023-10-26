af3c282243b5b9650073e1baa1597a45fdc02e84
JellyPumps <sarthakraibhartiya@gmail.com>
2023-10-26 17:21:49 +0530

    Updated to godot 4
    Code changes to fit godot 4 syntax.

--- gameplay/scripts/world.gd
+++ gameplay/scripts/world.gd
@@ -4,8 +4,8 @@ extends Node2D
 var current_level : int = 1
 
 # Objects
-onready var h_box_container : HBoxContainer = $CanvasLayer/Control/VBoxContainer/HBoxContainer
-onready var canvas_layer = $CanvasLayer
+@onready var h_box_container : HBoxContainer = $CanvasLayer/Control/VBoxContainer/HBoxContainer
+@onready var canvas_layer = $CanvasLayer
 
 # Dictionaries
 var weapon_affinity = {
@@ -17,24 +17,9 @@ var weapon_affinity = {
 
 func _ready():
 	for btn in h_box_container.get_children():
-		btn.connect("pressed", self, "_on_Affinity_pressed", [btn])
+		btn.connect("pressed", Callable(self, "_on_Affinity_pressed").bind(btn))
 
 func _on_Affinity_pressed(which : Button): 
 	var colon_index : int = str(which).find(":")
 	if colon_index == -1: return
 	var button = str(which).substr(colon_index + 1, str(which).length())
-	
-	canvas_layer.visible = false
-	load_level()
-	
-	var player = get_node("level/player")
-	player.affinity = weapon_affinity[button]
-	
-	print(player.affinity)
-
-func load_level():
-	var level_path : String      = "res://gameplay/scenes/levels/level_"+str(current_level)+".tscn"
-	var level      : PackedScene = load(level_path)
-	
-	var level_instance = level.instance()
-	add_child(level_instance)
