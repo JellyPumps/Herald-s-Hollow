d3cf6143e08b610a5a86b219780a65f72bb98303
JellyPumps <sarthakraibhartiya@gmail.com>
2023-11-13 15:09:41 +0530

    Updated how affinity selection is handled
    Currently does not apply affinity to player.

--- gameplay/scripts/world.gd
+++ gameplay/scripts/world.gd
@@ -7,19 +7,26 @@ var current_level : int = 1
 @onready var h_box_container : HBoxContainer = $CanvasLayer/Control/VBoxContainer/HBoxContainer
 @onready var canvas_layer = $CanvasLayer
 
-# Dictionaries
-var weapon_affinity = {
-	"[Button:1279]" : "Sword",
-	"[Button:1280]" : "Bow",
-	"[Button:1281]" : "Sorcery",
-	"[Button:1282]" : "Holy"
-}
+# Arrays
+var delimiters = [":",";"]
 
 func _ready():
 	for btn in h_box_container.get_children():
-		btn.connect("pressed", Callable(self, "_on_Affinity_pressed").bind(btn))
+		btn.connect("pressed", Callable(self, "set_affinity").bind(btn))
 
-func _on_Affinity_pressed(which : Button): 
-	var colon_index : int = str(which).find(":")
-	if colon_index == -1: return
-	var button = str(which).substr(colon_index + 1, str(which).length())
+func set_affinity(button_id : Button):
+	var id : String = split_button_id(str(button_id), delimiters[0])
+	
+	# Set affinity here
+	print(id)
+	
+	# Disconnect signal
+	button_id.disconnect("pressed", Callable(self, "set_affinity").bind(button_id))
+	
+	# Disable canvas layer after selection
+	canvas_layer.visible = false
+	
+	return
+
+func split_button_id(subject_id : String, delimiter : String):
+	return subject_id.rsplit(delimiter, true, 1)[0]
